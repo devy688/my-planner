a05359e6f8f3f8f728db357ecbd62ded5a5c0dc9
devy688 <devyoung688@gmail.com>
2024-05-23 17:08:00 +0900

    [UI/UX] List에서 Calendar 분리

---
+++ src/components/Calendar/Calendar.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import RenderHeader from './RenderHeader';
+import RenderDays from './RenderDays';
+import RenderCells from './RenderCells';
+import { addMonths, subMonths } from 'date-fns';
+import './Calendar.css';
+
+export default function Calendar() {
+    const [currentMonth, setCurrentMonth] = useState(new Date());
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
+    const prevMonth = () => {
+        setCurrentMonth(subMonths(currentMonth, 1));
+    };
+    const nextMonth = () => {
+        setCurrentMonth(addMonths(currentMonth, 1));
+    };
+
+    const onDateClick = (day) => {
+        setSelectedDate(day);
+    };
+
+    return (
+        <div className='calendar'>
+            <RenderHeader
+                currentMonth={currentMonth}
+                prevMonth={prevMonth}
+                nextMonth={nextMonth}
+            />
+            <RenderDays />
+            <RenderCells
+                currentMonth={currentMonth}
+                selectedDate={selectedDate}
+                onDateClick={onDateClick}
+            />
+        </div>
+    );
+}
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,44 +1,14 @@
-import { useState } from 'react';
-import RenderHeader from '../../components/Calendar/RenderHeader';
-import RenderDays from '../../components/Calendar/RenderDays';
-import RenderCells from '../../components/Calendar/RenderCells';
+import Calendar from '../Calendar/Calendar';
 import TodoForm from '../TodoForm/TodoForm';
-import { addMonths, subMonths } from 'date-fns';
 import './List.css';
 
 export default function List(props) {
     let { todoData } = props;
 
-    const [currentMonth, setCurrentMonth] = useState(new Date());
-    const [selectedDate, setSelectedDate] = useState(new Date());
-
-    const prevMonth = () => {
-        setCurrentMonth(subMonths(currentMonth, 1));
-    };
-    const nextMonth = () => {
-        setCurrentMonth(addMonths(currentMonth, 1));
-    };
-
-    const onDateClick = (day) => {
-        setSelectedDate(day);
-    };
-
     return (
         <div className='todo-container'>
             <div className='task-calendar-layout'>
-                <div className='calendar'>
-                    <RenderHeader
-                        currentMonth={currentMonth}
-                        prevMonth={prevMonth}
-                        nextMonth={nextMonth}
-                    />
-                    <RenderDays />
-                    <RenderCells
-                        currentMonth={currentMonth}
-                        selectedDate={selectedDate}
-                        onDateClick={onDateClick}
-                    />
-                </div>
+                <Calendar />
             </div>
             <div className='task-list-layout'>
                 <div className='todo-form'>
